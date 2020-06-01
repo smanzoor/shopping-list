d9e0e4eb5e5cdebee54e57cd9e5c45d395bf897c
Shahid Manzoor <shahidmanzoor@Shahids-MacBook-Pro.local>
2020-06-01 02:15:20 -0500

    initial commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShippingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
